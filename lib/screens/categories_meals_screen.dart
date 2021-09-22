b05d49cfc2c4d3e851c20d68a8759e07960c3c32
Adriano Gomes <adriano.gomes@gsw.com.br>
2021-09-21 21:55:24 -0300

    Criando rotas nomeadas com argumentos;

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  final Category category;
-
-  const CategoriesMealsScreen(this.category);
-
+  
   @override
   Widget build(BuildContext context) {
+
+    final category = ModalRoute.of(context)!.settings.arguments as Category;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(this.category.title),
+        title: Text(category.title),
       ),
       body: Center(
-        child: Text('Receitas por Categoria ${this.category.id}'),
+        child: Text('Receitas por Categoria ${category.id}'),
       ),
     );
   }
