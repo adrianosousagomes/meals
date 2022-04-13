2c894b688995c056522ad1c7a5de4ddf000d6004
Adriano Gomes <kapiturasp@gmail.com>
2022-04-13 00:02:35 -0300

    - Finalizando aula 204; - Implementando tela de configurações com os filtros;

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,19 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:meals/components/main_drawer.dart';
+import 'package:meals/models/settings.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  var settings = Settings();
+
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    Function(bool) onChange,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: onChange,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Configurações'),
-      ),
-      drawer: MainDrawer(),
-      body: Center(
-        child: Text('Configurações'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Configurações'),
+        ),
+        drawer: MainDrawer(),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Configurações',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            Expanded(
+                child: ListView(
+              children: [
+                _createSwitch(
+                  'Sem Glutén',
+                  'Só exibe refeições sem glutén!',
+                  settings.isGlutenFree,
+                  (value) => setState(() => settings.isGlutenFree = value),
+                ),
+                _createSwitch(
+                  'Sem Lactose',
+                  'Só exibe refeições sem lactose!',
+                  settings.isGlutenFree,
+                  (value) => setState(() => settings.isLactoseFree = value),
+                ),
+                _createSwitch(
+                  'Vegana',
+                  'Só exibe refeições veganas!',
+                  settings.isGlutenFree,
+                  (value) => setState(() => settings.isVegan = value),
+                ),
+                _createSwitch(
+                  'Vegetariana',
+                  'só exibe refeições vegetarianas!',
+                  settings.isGlutenFree,
+                  (value) => setState(() => settings.isVegetarian = value),
+                )
+              ],
+            ))
+          ],
+        ));
   }
 }
